59d73b96c0b87a2e3722170159996501e80bfd21
anikahammedkhan <anikkhan1105@gmail.com>
2024-12-11 18:17:08 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useRef, useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="toolbar">
+        <button>Pen</button>
+        <button>Erase</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
